e44beac969d91c39db6b4e9833eef09c730bc389
peit <pi6073@hs-el.de>
2023-04-06 15:26:11 +0200

    added new super contrast "read" option

---
+++ gnomeTerminal/Corvus-read.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+# Gnome Terminal color theme install script.
+# Adapted from chriskempson's base16-gnome-termianl: 
+# https://github.com/chriskempson/base16-gnome-terminal/blob/master/base16-3024.dark.sh
+# Created with ThemeCreator, https://github.com/mswift42/themecreator
+
+# Author: Piet Marx
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Corvus-read" 
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="Corvus-read" 
+[[ -z "$DCONF" ]] && DCONF=dconf 
+[[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen 
+
+dset() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    if [[ "$type" == "string" ]]; then
+        val="'$val'"
+    fi
+
+    "$DCONF" write "$PROFILE_KEY/$key" "$val"
+}
+
+# Because dconf still doesn't have "append"
+dlist_append() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$DCONF" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "'$val'"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$DCONF" write "$key" "[$entries]"
+}
+
+# Newest versions of gnome-terminal use dconf
+if which "$DCONF" > /dev/null 2>&1; then
+    # Check that uuidgen is available
+    type $UUIDGEN >/dev/null 2>&1 || { echo >&2 "Requires uuidgen but it's not installed.  Aborting!"; exit 1; }
+
+    [[ -z "$BASE_KEY_NEW" ]] && BASE_KEY_NEW=/org/gnome/terminal/legacy/profiles:
+
+    if [[ -n "`$DCONF list $BASE_KEY_NEW/`" ]]; then
+        if which "$UUIDGEN" > /dev/null 2>&1; then
+            PROFILE_SLUG=`uuidgen`
+        fi
+
+        if [[ -n "`$DCONF read $BASE_KEY_NEW/default`" ]]; then
+            DEFAULT_SLUG=`$DCONF read $BASE_KEY_NEW/default | tr -d \'`
+        else
+            DEFAULT_SLUG=`$DCONF list $BASE_KEY_NEW/ | grep '^:' | head -n1 | tr -d :/`
+        fi
+
+        DEFAULT_KEY="$BASE_KEY_NEW/:$DEFAULT_SLUG"
+        PROFILE_KEY="$BASE_KEY_NEW/:$PROFILE_SLUG"
+
+        # Copy existing settings from default profile
+        $DCONF dump "$DEFAULT_KEY/" | $DCONF load "$PROFILE_KEY/"
+
+        # Add new copy to list of profiles
+        dlist_append $BASE_KEY_NEW/list "$PROFILE_SLUG"
+
+        # Update profile values with theme options
+        dset visible-name "'$PROFILE_NAME'"
+        dset palette "['#1f273d', '#d6dbeb','#ecf0ff', '#c4c8d6', '#b1b5c2', '#93e8f4', '#d665c4', '#a597da', '#7eccf6', '#e396f3', '#d665c4',  '#ec2ba8', '#93e8f4', '#b1b5c2', '#d6dbeb', '#e9eeff']"
+        dset background-color "'#1f273d'"
+        dset foreground-color "'#e9eeff'"
+        dset bold-color "'#e9eeff'"
+        dset bold-color-same-as-fg "true"
+        dset cursor-colors-set "true"
+        dset cursor-background-color "'#e9eeff'"
+        dset cursor-foreground-color "'#1f273d'"
+        dset use-theme-colors "false"
+        dset use-theme-background "false"
+
+        unset PROFILE_NAME
+        unset PROFILE_SLUG
+        unset DCONFrg
+        unset UUIDGEN
+        exit 0
+    fi
+fi
+
+# Fallback for Gnome 2 and early Gnome 3
+[[ -z "$GCONFTOOL" ]] && GCONFTOOL=gconftool
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/gnome-terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+gset() {
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
+
+    "$GCONFTOOL" --set --type "$type" "$PROFILE_KEY/$key" -- "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$GCONFTOOL" --get "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "$val"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$GCONFTOOL" --set --type list --list-type $type "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
+
+gset string visible_name "$PROFILE_NAME"
+gset string palette "#1f273d:#d6dbeb:#ecf0ff:#c4c8d6:#b1b5c2:#93e8f4:#d665c4:#e396f3:#7eccf6:#93e8f4:#a597da:#ec2ba8:#e396f3:#b1b5c2:#d6dbeb:#e9eeff"
+gset string background_color "#1f273d"
+gset string foreground_color "#e9eeff"
+gset string bold_color "#e9eeff"
+gset bool   bold_color_same_as_fg "true"
+gset bool   cursor-colors-set "true"
+gset string cursor-background-color "'#e9eeff'"
+gset string cursor-foreground-color "'#1f273d'"
+gset bool   use_theme_colors "false"
+gset bool   use_theme_background "false"
+
+unset PROFILE_NAME
+unset PROFILE_SLUG
+unset DCONF
+unset UUIDGEN
